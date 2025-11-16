4dd4e2ac27158e4a9c89f6f46276d95bbd4cabec
fangyuqi <857559565@qq.com>
2025-11-16 15:20:30 +0800

    init coordinator

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,63 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
 
 type Coordinator struct {
 	// Your definitions here.
+	mu                sync.Mutex
+	nMap              int           // number of map tasks
+	nReduce           int           // number of reduce tasks
+	mapTasks          []*TaskStatus // "idle", "in-progress", "completed"
+	reduceTasks       []*TaskStatus // "idle", "in-progress", "completed"
+	intermediateFiles [][]string    // intermdediate[mapTaskIndex][reduceTaskIndex] = filename
+	alldDone          bool
+	mapTaskStart      map[int]time.Time // mapTaskStart[mapTaskIndex] = timestamp
+	reduceTaskStart   map[int]time.Time // reduceTaskStart[reduceTaskIndex] = timestamp
+}
 
+type TaskType int
+
+const (
+	MapTask TaskType = iota
+	ReduceTask
+	DoneTask
+)
+
+type TaskStatus int
+
+const (
+	Idle TaskStatus = iota
+	InProgress
+	Completed
+)
+
+type Task struct {
+	Type      TaskType
+	Index     int      // map task index or reduce task index
+	Inputfile []string // for map task
+	NMap      int      // number of map tasks
+	NReduce   int      // number of reduce tasks
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +71,41 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
 	// Your code here.
+	c.nMap = len(files)
+	c.nReduce = nReduce
+	c.mapTasks = make([]*TaskStatus, c.nMap)
+	c.reduceTasks = make([]*TaskStatus, c.nReduce)
+	c.intermediateFiles = make([][]string, c.nMap)
+	c.mapTaskStart = make(map[int]time.Time)
+	c.reduceTaskStart = make(map[int]time.Time)
 
+	for i := range c.mapTasks {
+		c.mapTasks[i] = new(TaskStatus)
+		*c.mapTasks[i] = Idle
+		c.intermediateFiles[i] = make([]string, c.nReduce)
+	}
+
+	for i := range c.reduceTasks {
+		c.reduceTasks[i] = new(TaskStatus)
+		*c.reduceTasks[i] = Idle
+	}
 
 	c.server()
 	return &c
