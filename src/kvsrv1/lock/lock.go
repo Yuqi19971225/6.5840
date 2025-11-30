f4f6b137b6acb767d40442a5031c4cf21ae69a7d
fangyuqi <857559565@qq.com>
2025-11-30 16:01:48 +0800

    finish lab2 lock

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,6 +1,7 @@
 package lock
 
 import (
+	"6.5840/kvsrv1/rpc"
 	"6.5840/kvtest1"
 )
 
@@ -11,6 +12,8 @@ type Lock struct {
 	// MakeLock().
 	ck kvtest.IKVClerk
 	// You may add code here
+	key    string
+	selfId string
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -19,15 +22,54 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
+	lk := &Lock{
+		ck:     ck,
+		key:    l,
+		selfId: kvtest.RandValue(8),
+	}
 	// You may add code here
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
+	for {
+		value, version, err := lk.ck.Get(lk.key)
+		if err == rpc.ErrNoKey {
+			if lk.ck.Put(lk.key, lk.selfId, 0) == rpc.OK {
+				return
+			}
+			continue
+		}
+		if err == rpc.OK {
+			if value == lk.selfId {
+				return
+			}
+			if value != "" {
+				continue
+			}
+			if lk.ck.Put(lk.key, lk.selfId, version) == rpc.OK {
+				return
+			}
+		}
+	}
+
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	for {
+		value, version, err := lk.ck.Get(lk.key)
+		if err == rpc.ErrNoKey {
+			return
+		}
+		if err == rpc.OK && value == lk.selfId {
+			err := lk.ck.Put(lk.key, "", version)
+			if err == rpc.OK {
+				return
+			}
+		} else {
+			return
+		}
+	}
 }
